b53b53396e08ceb7d430ede13708099b60cd9ac8
V1tale <elativ.c@gmail.com>
2022-12-13 09:44:58 +0100

    completato senza bonus

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ require_once __DIR__ . "/articles.php";
                         <div class="card">
                             <img src="./img/<?php echo ($food->picture) ?>.jpg" alt="">
                             <h3><?php echo ($food->name) ?></h3>
+                            <h4>Category: <?php echo $food->category->pet ?></h4>
                             <h4>Price: € <?php echo ($food->price) ?></h4>
                             <h4>Chewable: <?php echo ($food->available) ?></h4>
                             <h4>Quantity: <?php echo ($food->quantity) ?> kg</h4>
@@ -42,6 +43,7 @@ require_once __DIR__ . "/articles.php";
                         <div class="card">
                             <img src="./img/<?php echo ($toy->picture) ?>.jpg" alt="">
                             <h3><?php echo ($toy->name) ?></h3>
+                            <h4>Category: <?php echo $toy->category->pet ?></h4>
                             <h4>Price: € <?php echo ($toy->price) ?></h4>
                             <h4>Chewable: <?php echo ($toy->available) ?></h4>
                             <h4>Color: <?php echo ($toy->color) ?></h4>
@@ -58,11 +60,12 @@ require_once __DIR__ . "/articles.php";
                         <div class="card">
                             <img src="./img/<?php echo ($kennel->picture) ?>.jpg" alt="">
                             <h3><?php echo ($kennel->name) ?></h3>
+                            <h4>Category: <?php echo $kennel->category->pet ?></h4>
                             <h4>Price: € <?php echo ($kennel->price) ?></h4>
                             <h4>available: <?php echo ($kennel->available) ?></h4>
                             <h4>color: <?php echo ($kennel->color) ?></h4>
-                            <h4>height: <?php echo ($kennel->height) ?></h4>
-                            <h4>width: <?php echo ($kennel->width) ?></h4>
+                            <h4>height: <?php echo ($kennel->height) ?> m</h4>
+                            <h4>width: <?php echo ($kennel->width) ?> m</h4>
                         </div>
                     </div>
                 <?php } ?>
